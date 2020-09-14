dff278c585eb6a6aad15877e34179ebbd20fd804
AmrAhmedd <amr.aboelnaga@student.guc.edu.eg>
2020-09-14 05:34:51 +0200

    first commit

---
+++ src/main/java/com/rest/webservices/basic/auth/AuthenticationBean.java
@@ -0,0 +1,31 @@
+package com.rest.webservices.basic.auth;
+
+public class AuthenticationBean {
+	private String message;
+	
+	
+	public AuthenticationBean(String message) {
+		this.message=message;
+		
+	}
+
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {		this.message = message;
+	}
+
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+	
+	
+	
+}
